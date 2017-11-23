49b01bf4981d55a4fae14e9c1e881583616904a1
Kyle <Kingallsmh@gmail.com>
2017-11-23 15:51:10 -0400

    Add new project to new Repo

---
+++ Assets/Script/UtilScripts/ComFunc.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComFunc {
+
+    float currentTime = 0;
+
+	public float LerpForTime(float x, float y, float forTime)
+    {
+        return Mathf.Lerp(x, y, currentTime/forTime);
+    }
+}
